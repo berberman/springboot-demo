92ddec8445fcecf97f235f96ac14c776af3c0ba6
berberman <1793913507@qq.com>
2018-02-27 22:42:06 +0800

    :tada: Initial commit

---
+++ src/main/kotlin/cn/berberman/springbootdemo/SpringBootDemoApplication.kt
@@ -0,0 +1,11 @@
+package cn.berberman.springbootdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootDemoApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(SpringBootDemoApplication::class.java, *args)
+}
